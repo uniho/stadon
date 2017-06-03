0099db35c3347acacf246241eefa9e103451c796
uniho <uniho@fc4.so-net.ne.jp>
2017-06-03 11:01:57 +0900

    misc

--- source/script/tablets/01.txt.js
+++ source/script/tablets/01.txt.js
@@ -1,3 +1,3 @@
 
-return "Hail a taxi, find a recipe, run through a temple ? Google Play has all the apps and games that let you make your Android device uniquely yours.";
+return "A clean, simple, customisable home screen that comes with the power of Google Now: Traffic alerts, weather and much more, just a swipe away.";
 
